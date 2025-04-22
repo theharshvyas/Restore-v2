ef3264632e984a224d1da06e91ef10b3372d79cf
theharshvyas <harshvyas@live.com>
2025-04-23 03:23:29 +0530

    End of section 12 updated

--- API/DTOs/OrderItemDto.cs
+++ API/DTOs/OrderItemDto.cs
@@ -3,8 +3,8 @@ namespace API.DTOs;
 public class OrderItemDto
 {
     public int ProductId { get; set; }
-    public string Name { get; set; }
-    public string PictureUrl { get; set; }
+    public required string Name { get; set; }
+    public required string PictureUrl { get; set; }
     public long Price { get; set; }
     public int Quantity { get; set; }
 }
